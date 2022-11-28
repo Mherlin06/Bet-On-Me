e258d308fcbf54167d78ab5e8b8184e614649755
Hugues Mourice <mourice.hugues@gmail.com>
2022-11-28 21:39:27 +0100

    add pages inside router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import { Layout } from "./Layout/Layout";
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home/Home";
+import Courses from "./pages/Courses/Courses";
+import Experiences from "./pages/Experiences/Experiences";
+import Error from "./pages/Error/Error";
 
 function App() {
   return (
     <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route exact path="/"/>
-      </Routes>
-    </Layout>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/formations" element={<Courses />} />
+          <Route exact path="/experiences" element={<Experiences/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
