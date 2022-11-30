a814b77cceb6352ac61bc45a7e1df89ec0b8e8b3
Hugues Mourice <mourice.hugues@gmail.com>
2022-11-30 09:47:22 +0100

    add data to courses.js

---
+++ src/data/courses.js
@@ -0,0 +1,24 @@
+const courses = [
+  {
+    title: "Développeur Web",
+    level: "titre RNCP niveau 5 (Bac +2)",
+    date: 2022,
+    school: "Openclassrooms",
+  },
+  {
+    title: "DUT Génie Biologique",
+    level: "titre RNCP niveau 5 (Bac +2)",
+    date: 2011,
+    school: "IUT Toulon la Garde",
+  },
+  {
+    title: "BAC STL option Génie Biologique",
+    grade: "mention Assez Bien",
+    date: 2009,
+    school: "Lycée Jules Ferry, Cannes",
+  },
+];
+
+export function getCourses(){
+    return courses;
+} 
