27ed611080e1db540d4dc33908b2f08df721453b
Hugues Mourice <mourice.hugues@gmail.com>
2022-12-07 17:41:08 +0100

    added style to <header> & <contactbutton>

---
+++ src/components/ContactButton/ContactButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactButton = ({ label }) => {
+  return <button className="contact-button">{label}</button>;
+};
+
+export default ContactButton;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import ContactButton from "../ContactButton/ContactButton";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <h1>Bet On Me!</h1>
+      <div>
+        <p>Hugues Mourice</p>
+        <p>31 ans</p>
+        <p>Développeur ReactJS</p>
+      </div>
+      <ContactButton label="Me contacter"/>
+    </header>
+  );
+};
 
 export default Header;
